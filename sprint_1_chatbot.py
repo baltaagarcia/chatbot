17cdc953b15f1cb4c2ea51cc78eb24b8a8980242
baltaagarcia <165027582+baltaagarcia@users.noreply.github.com>
2025-05-06 16:41:08 -0300

    Add files via upload

---
+++ sprint_1_chatbot.py
@@ -0,0 +1,33 @@
+
+import csv
+def leer_preguntas_csv(nombre_archivo):
+    preguntas = {}
+
+    with open(nombre_archivo, newline='', encoding='utf-8') as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            pregunta = fila['pregunta'].lower()
+            respuesta = fila['respuesta'].strip()
+            preguntas[pregunta] = respuesta
+
+
+
+def buscar_pregunta(preguntas, consulta_usuario):
+    consulta_usuario = consulta_usuario.lower()
+    if consulta_usuario in preguntas:
+        return preguntas[consulta_usuario]
+    else:
+        return "No tengo la informacion para responder esta consulta"
+    
+
+
+
+
+def main():
+    nombre_archivo = 'preguntas.csv'
+    preguntas = leer_preguntas_csv(nombre_archivo)
+    input_usuario= ""
+    while input_usuario.lower != "chau":
+        input_usuario=input("Bienvenido al ChatBot! Respondere tus consultas")
+        
+
